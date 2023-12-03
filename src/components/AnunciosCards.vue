8adab4e586d864c309ea7344fb61ad481018c321
OscarIgM <90069685+OscarIgM@users.noreply.github.com>
2023-12-03 00:33:53 -0300

    magias

--- src/components/AnunciosCards.vue
+++ src/components/AnunciosCards.vue
@@ -1,7 +1,7 @@
 <template>
   <div class="col-md-4 d-flex mb-4">
     <div class="card shadow" style="height: 30rem; width: 20rem;">
-      <img :src="obtenerUrlImagen(producto.imageId)" class="card-img-top row mt-3 mx-auto w-50" style="width: 100%; height: 160px; object-fit: cover;" alt="...">
+      <img :src="imagenProducto" class="card-img-top row mt-3 mx-auto w-50" style="width: 100%; height: 160px; object-fit: cover;" alt="...">
       <div class="card-body" style="position: relative;">
         <router-link :to="`/product/${producto.id}`">
         <h5 class="card-title d-flex justify-content-center" style="font-size: 1rem; overflow: hidden; white-space: nowrap; text-overflow: ellipsis;">
@@ -26,8 +26,10 @@ import Stars from "./mini_components/Stars.vue";
 import { defineProps } from 'vue';
 import axios from 'axios';
 import { RouterLink } from 'vue-router';
+import {ref, onMounted }from 'vue';
 
 const { producto } = defineProps(['producto']);
+const imagenProducto = ref('');
 
 const obtenerUrlImagen = async (imageId) => {
   try {
@@ -41,14 +43,17 @@ const obtenerUrlImagen = async (imageId) => {
         ''
       )
     );
-
-    return `data:${response.headers['content-type'].toLowerCase()};base64,${base64}`;
+    const url = `data:${response.headers['content-type'].toLowerCase()};base64,${base64}`;
+    imagenProducto.value = url;
   } catch (error) {
     console.error('Error al obtener la imagen', error);
     // Puedes manejar el error o mostrar una imagen por defecto
-    return 'URL_IMAGEN_POR_DEFECTO';
+    imagenProducto.value = 'URL_IMAGEN_POR_DEFECTO';
   }
 };
+onMounted(() => {
+  obtenerUrlImagen(producto.imageId);
+});
 
 </script>
 
