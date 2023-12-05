1db693a6489167e69f6423a2f4dc799ed61a6218
OscarIgM <90069685+OscarIgM@users.noreply.github.com>
2023-12-05 02:38:46 -0300

    paginacion lista

--- src/components/mini_components/Pagination.vue
+++ src/components/mini_components/Pagination.vue
@@ -1,11 +1,33 @@
 <template>
-      <div class="d-flex justify-content-center mt-3">
-                            <ul class="pagination">
-                                <li class="page-item"><a class="page-link" href="#">Previous</a></li>
-                                <li class="page-item"><a class="page-link" href="#">1</a></li>
-                                <li class="page-item"><a class="page-link" href="#">2</a></li>
-                                <li class="page-item"><a class="page-link" href="#">3</a></li>
-                                <li class="page-item"><a class="page-link" href="#">Next</a></li>
-                            </ul>
-                        </div>
-</template>
+    <div class="d-flex justify-content-center mt-3">
+      <ul class="pagination">
+        <li class="page-item">
+          <a class="page-link" href="#" @click="emitChangePage('prev')">Previous</a>
+        </li>
+        <li class="page-item" v-for="page in Math.min(totalPages || 1, 5)" :key="page" :class="{ active: page === currentPage }">
+          <a class="page-link" href="#" @click="emitChangePage(page)">{{ page }}</a>
+        </li>
+        <li class="page-item">
+          <a class="page-link" href="#" @click="emitChangePage('next')">Next</a>
+        </li>
+      </ul>
+    </div>
+  </template>
+  <script setup>
+  import { defineProps, defineEmits } from 'vue';
+  
+  const { currentPage, totalPages } = defineProps(['currentPage', 'totalPages']);
+  const emit = defineEmits();
+  
+  const emitChangePage = (page) => {
+    if (page === 'prev' && currentPage > 1) {
+      emit('changePage', currentPage - 1);
+    } else if (page === 'next' && currentPage < totalPages) {
+      emit('changePage', currentPage + 1);
+    } else if (typeof page === 'number') {
+      emit('changePage', page);
+    }
+  };
+  </script>
+  
+  
--- src/views/Logged/HomepageLogged.vue
+++ src/views/Logged/HomepageLogged.vue
@@ -2,38 +2,56 @@
   <div>
     <NavBarLogeado :searchTerm="searchTerm"></NavBarLogeado>
     <div style="position: relative; overflow: hidden;">
-        <CarouselHomePage></CarouselHomePage>
+      <CarouselHomePage></CarouselHomePage>
     </div>
     <div class="row col-7 mx-auto gy-0 py-5" style="display: flex;">
       <AnunciosCards
-        v-for="producto in productos"
+        v-for="producto in paginatedProducts"
         :key="producto.id"
         :producto="producto"
       />
     </div>
+
+    <Pagination :currentPage="currentPage" :totalPages="totalPages" @changePage="changePage"></Pagination>
   </div>
-<Pagination></Pagination>
 </template>
 <script setup>
 import AnunciosCards from '../../components/AnunciosCards.vue';
 import Pagination from '../../components/mini_components/Pagination.vue';
-import { ref } from 'vue';
-import axios from 'axios'; 
+import { ref, computed } from 'vue';
+import axios from 'axios';
 import NavBarLogeado from '../../components/NavBarLogeado.vue';
 import { onMounted } from 'vue';
 import CarouselHomePage from '../../components/CarouselHomePage.vue';
 
 const productos = ref([]);
+const currentPage = ref(1);
+const pageSize = ref(6);
+const totalPages = ref(1);
+const searchTerm = ref(""); 
 
-(async () => {
+onMounted(async () => {
+  await loadProducts();
+});
+
+const loadProducts = async () => {
   try {
     const response = await axios.get('http://localhost:8080/products');
     productos.value = response.data;
+    totalPages.value = Math.ceil(response.data.length / pageSize.value);
   } catch (error) {
     console.error('Error al cargar productos:', error);
   }
-})();
+};
 
+const paginatedProducts = computed(() => {
+  const startIndex = (currentPage.value - 1) * pageSize.value;
+  const endIndex = startIndex + pageSize.value;
+  return productos.value.slice(startIndex, endIndex);
+});
 
+const changePage = async (page) => {
+  currentPage.value = page;
+};
 
 </script>
